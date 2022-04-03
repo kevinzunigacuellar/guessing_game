bae33648b7cbc40c4caed1584e5681acb64b7ea4
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-04-02 23:08:59 -0400

    create guessing game

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+use colored::*;
+
+fn main() {
+    // 0. Welcome message
+    println!("Welcome to the guessing game! 🎉");
+    println!("================================");
+
+
+    // 1. Generate a secret number
+    let secret_num = rand::thread_rng().gen_range(0..50);
+    
+    // 2. Loop until the user guesses the secret number
+    loop {
+        // 2.1 Promp user for a number
+        println!("Please input your guess (0 - 50): ");
+        let mut guess = String::new();
+            io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        // 2.2 Convert the string to a number
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+    
+        println!("You guessed: {}", guess);
+
+        // 2.3 Compare the number to the secret number
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("{}","Your guess is too low!".red()),
+            Ordering::Greater => println!("{}","Your guess is too high!".red()),
+            Ordering::Equal => {
+                println!("{}", "Congratulations! You guessed right!".green());
+                break;
+            }
+        }
+    }
+}
